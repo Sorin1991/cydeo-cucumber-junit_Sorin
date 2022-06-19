942aaa2bb93ce4b38f763d0aaa704d552386d4a8
Sorin1991 <basco91@gmail.com>
2022-06-19 11:47:22 +0100

    Hooks take screenshot

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -29,6 +29,7 @@ public class Hooks {
     @After
     public void teardownScenario(Scenario scenario){
         // scenario.isFailed()--> if scenario fails method will return TRUE boolean value
+
         if(scenario.isFailed()) {
             byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshot, "image/png", scenario.getName());
